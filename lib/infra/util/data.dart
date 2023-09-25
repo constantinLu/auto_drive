9163b3dc8f3314f2240ed9ea414c5080aa20955d
Constantin Lungu <lunguucatalin@gmail.com>
2023-09-25 17:28:23 +0300

    AD-13 - added app name

--- lib/infra/util/data.dart
+++ lib/infra/util/data.dart
@@ -2,8 +2,8 @@ import 'package:auto_drive/domain/model/vehicle.dart';
 
 final audi = Vehicle(
     id: "asd",
-    name: "Bianca",
+    name: "SilverArrow",
     brand: "Audi",
     model: "A6 C6 AVANT",
     modelDetails: "2008, BRF",
-    vin: "Wauzzz4f48n067440".toUpperCase());
+    vin: "Wauzzz5742a157440".toUpperCase());
